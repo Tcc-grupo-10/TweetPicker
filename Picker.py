ca112a1bed3e70f7fd3b68ae9923c173def873bc
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2017-08-24 13:15:16 -0300

    Starting OO paradigm

--- Picker.py
+++ Picker.py
@@ -1,32 +1,10 @@
-import base64
 import urllib
-import urllib2
-import json
+import TwitterIntegration
 
 #SearchHotkeys
 rawKey = "game of thrones"
 searchEncoded = urllib.quote(rawKey)
 
-#Twitter's keys
-consumerKey = "9cuNRRcqj3wnd7jrUw7Rw1DKw"
-consumerSecret = "KZ66L3zmIxQdW0LSwcCoyAN0iXCzusnQTGDmEz6vwAGhq4cFss"
-
-#Twitter's API Token
-base64Secret = "Basic " + base64.b64encode(consumerKey+":"+consumerSecret)
-
-#Getting a usable token "userless"
-url = 'https://api.twitter.com/oauth2/token'
-data = urllib.urlencode({'grant_type': 'client_credentials'})
-req = urllib2.Request(url, data)
-req.add_header('Authorization', base64Secret)
-response = urllib2.urlopen(req).read()
-jsonObj = json.loads(response)
-bearerToken = "Bearer " + jsonObj.get("access_token", "none")
-
-#Searching on Twitter
-url = 'https://api.twitter.com/1.1/search/tweets.json?q='+searchEncoded
-req = urllib2.Request(url)
-req.add_header('Authorization', bearerToken)
-response = urllib2.urlopen(req).read()
-searchResultObj = json.loads(response)
-print(searchResultObj)
+tokenUserless = TwitterIntegration.getTokenUserless()
+searchRaw = TwitterIntegration.getSearch(searchEncoded, tokenUserless)
+print searchRaw
---
+++ TwitterIntegration.py
@@ -0,0 +1,29 @@
+import base64
+import urllib
+import urllib2
+import json
+
+#Twitter's keys
+consumerKeyTwitter = "9cuNRRcqj3wnd7jrUw7Rw1DKw"
+consumerSecretTwitter = "KZ66L3zmIxQdW0LSwcCoyAN0iXCzusnQTGDmEz6vwAGhq4cFss"
+
+#Twitter's API Token
+base64Secret = "Basic " + base64.b64encode(consumerKeyTwitter+":"+consumerSecretTwitter)
+
+
+def getTokenUserless():
+    url = 'https://api.twitter.com/oauth2/token'
+    data = urllib.urlencode({'grant_type': 'client_credentials'})
+    req = urllib2.Request(url, data)
+    req.add_header('Authorization', base64Secret)
+    response = urllib2.urlopen(req).read()
+    jsonObj = json.loads(response)
+    return "Bearer " + jsonObj.get("access_token", "none")
+
+
+def getSearch(searchEncoded, bearerToken):
+    url = 'https://api.twitter.com/1.1/search/tweets.json?q='+searchEncoded
+    req = urllib2.Request(url)
+    req.add_header('Authorization', bearerToken)
+    response = urllib2.urlopen(req).read()
+    return json.loads(response)
