997d6d239a19b46b8e430b8e9ad8b83d1475b748
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2017-08-24 16:41:20 -0300

    TODO's

--- Picker.py
+++ Picker.py
@@ -21,3 +21,11 @@ for x in range(0, 15):
         searchRaw = TwitterIntegration.getNextSearch(nextUrl, tokenUserless)
         nextUrl = TwitterTimelineIterator.saveNext(searchRaw, rawKey)
 
+
+#TODO - Set a timmer of 15 minutes and run the "for" again
+#TODO - Check if we just can call the search 15 times or is 180
+#TODO - Change the count of TwitterIntegration to 100 - (1 - to Test)
+#TODO - Change DynamoDB from Local to the Cloud (using env vars)
+#TODO - Write the README
+#TODO - If we have time, change to Python 3 (current 2.7)
+#TODO - If we have time, use "user token" - browser needed
