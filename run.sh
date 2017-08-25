37cfd975c89bc2123deacff5b6c964eb4c900b72
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2017-08-25 16:21:36 -0300

    run mac/linux

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export consumer_key_twitter=PLACEHOLDER
+export consumer_secret_twitter=PLACEHOLDER
+export access_key=PLACEHOLDER
+export secret_access=PLACEHOLDER
+
+python Picker.py
+
+read -p " << -- Press enter to continue -- >>" nothing
