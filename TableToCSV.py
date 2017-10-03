a8871ba5dcbc9184fc911af5e444c32fe9d9794e
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2017-10-03 15:51:43 -0300

    csv creator

---
+++ TableToCSV.py
@@ -0,0 +1,30 @@
+import boto3
+import os
+import DatabaseCreator
+import csv
+
+accessKey = os.environ['access_key']
+secretAccess = os.environ['secret_access']
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb', region_name='sa-east-1', aws_access_key_id=accessKey, aws_secret_access_key=secretAccess)
+unTweeterizeTable = DatabaseCreator.unTweeterizeTable(dynamodb)
+
+response = unTweeterizeTable.scan()
+allTweets = response['Items']
+print len(allTweets)
+
+while 'LastEvaluatedKey' in response:
+    response = boto3.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+    allTweets.extend(response['Items'])
+    print len(allTweets)
+
+f = csv.writer(open("test.csv", "wb+"))
+f.writerow(["tweet_id", "search_key", "retweet_count", "favorite_count", "user_id", "text", "SPAM", "IRRELEVANTE"])
+for x in allTweets:
+    f.writerow([x["tweet_id"],
+                x["search_key"],
+                x["retweet_count"],
+                x["favorite_count"],
+                x["user_id"],
+                x["text"]])
--- UnTweeterize.py
+++ UnTweeterize.py
@@ -9,23 +9,24 @@ import emoji
 def processTweet(tweet):
     tweet = emoji.demojize(tweet, delimiters=(" :", ": "))
 
-    #To unicode
+    # To unicode
     tweet = unidecode(tweet)
     tweet = unicode(tweet)
 
-    #Convert to lower case
+    # Convert to lower case
     tweet = tweet.lower()
-    #Convert www.* or https?://* to URL
+    # Convert www.* or https?://* to URL
     tweet = re.sub('((www\.[^\s]+)|(https?:/[^\s]+))', '', tweet)
-    #Removing \n
+    # Removing \n
     tweet = tweet.replace('\n', ' ')
-    #Remove additional white spaces
+    # Remove additional white spaces
     tweet = re.sub('[\s]+', ' ', tweet)
-    #Replace #word with word
+    # Replace #word with word
     tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
-    #trim
-    tweet = tweet.strip('\'"')
+    # Trim
+    tweet = tweet.strip()
 
+    # Replacing unknown chars
     tweet = tweet.replace('[?]', '')
     tweet = tweet.replace('&amp;', '&')
     tweet = tweet.replace('&lt;', '<')
@@ -43,10 +44,10 @@ secretAccess = os.environ['secret_access']
 # Get the service resource.
 dynamodb = boto3.resource('dynamodb', region_name='sa-east-1', aws_access_key_id=accessKey, aws_secret_access_key=secretAccess)
 tweetRTTable = DatabaseCreator.tweetRTTable(dynamodb)
+# unTweeterizeTable = DatabaseCreator.unTweeterizeTable(dynamodb)
 
 response = tweetRTTable.scan()
 allTweets = response['Items']
-
 print len(allTweets)
 
 """while 'LastEvaluatedKey' in response:
@@ -56,3 +57,4 @@ print len(allTweets)
 
 for i in allTweets:
     print i["text"]
+    # Database.insertItem(i, unTweeterizeTable)
