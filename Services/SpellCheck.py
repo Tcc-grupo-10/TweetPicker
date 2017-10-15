1ee2f19154f2d752543a72592349d0601d90263f
ericcampa <ericcampanatti@hotmail.com>
2017-10-15 14:49:57 -0200

    spellChecker

--- Services/SpellCheck.py
+++ Services/SpellCheck.py
@@ -1,8 +1,31 @@
+import language_check
+import nltk
+import enchant
+import enchant.checker
+from enchant.checker.CmdLineChecker import CmdLineChecker
+
 
 def spellCheck(tweet):
+    tool = language_check.LanguageTool('en-US')
+    matches = tool.check(tweet)
+    while (len(matches) != 0):
+        tweetLog = tweet
+        tweet = language_check.correct(tweet, matches)
+        matches = tool.check(tweet)
+        if tweet == tweetLog:
+            break
+    return tweet
+
+def acronimCheck():
+    tweet = "teste"
+    enchant.add(tweet)
 
     return tweet
 
+def tokenizer():
+    tweet = ''
+    tokens = nltk.word_tokenize(tweet)
+    return tokens
 
 def grammarCheck(tweet):
 
@@ -23,3 +46,4 @@ def processTweet(tweet):
     tweet = orderCheck(tweet)
 
     return tweet
+
