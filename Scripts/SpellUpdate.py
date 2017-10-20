22df20d2d06224c6c8c286cba5b6ed9cdfc4cd3c
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2017-10-20 17:52:39 -0200

    SpellUpdate function

--- Scripts/SpellUpdate.py
+++ Scripts/SpellUpdate.py
@@ -1,15 +1,29 @@
-import os
 from Databases import Database
 from Services import SpellCheck
+
+
+def spellUpdate(tweet_id, clear_text, untweet_text):
+    Database.unTweeterizeTable.update_item(
+        Key={'tweet_id': tweet_id},
+        UpdateExpression="set clear_text=:c, untweet_text=:u",
+        ExpressionAttributeValues={
+            ':c': clear_text,
+            ':u': untweet_text
+        },
+        ReturnValues="UPDATED_NEW"
+    )
 
 allTweets = Database.getAll(Database.unTweeterizeTable)
 
 print len(allTweets)
 
 for tweet in allTweets:
-    tweet["untweet"] = tweet["clear_text"]
-    tweet["clear_text"] = SpellCheck.processTweet(tweet["untweet"])
+    tweet["untweet_text"] = tweet["clear_text"]
+    tweet["clear_text"] = SpellCheck.processTweet(tweet["untweet_text"])
 
     print tweet["untweet"]
     print tweet["clear_text"]
     print "\n\n"
+
+    # TODO -> JUST UNCOMMENT AFTER THE TESTS
+    # spellUpdate(tweet["tweet_id"], tweet["clear_text"], tweet["untweet_text"])
