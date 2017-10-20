4a75b73bfa04d7a3244fb26f24cb3a700513d3c7
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2017-10-20 18:45:29 -0200

    Table to CSV update

--- Scripts/TableToCSV.py
+++ Scripts/TableToCSV.py
@@ -1,32 +1,12 @@
 import csv
-import os
+from Databases import Database
 
-import boto3
+allTweets = Database.getAll(Database.unTweeterizeTable)
 
-from Databases import DatabaseCreator
-
-accessKey = os.environ['access_key']
-secretAccess = os.environ['secret_access']
-
-# Get the service resource.
-dynamodb = boto3.resource('dynamodb', region_name='sa-east-1', aws_access_key_id=accessKey, aws_secret_access_key=secretAccess)
-unTweeterizeTable = DatabaseCreator.unTweeterizeTable(dynamodb)
-
-response = unTweeterizeTable.scan()
-allTweets = response['Items']
-print len(allTweets)
-
-while 'LastEvaluatedKey' in response:
-    response = unTweeterizeTable.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
-    allTweets.extend(response['Items'])
-    print len(allTweets)
-
-f = csv.writer(open("test.csv", "wb+"))
-f.writerow(["tweet_id", "search_key", "retweet_count", "favorite_count", "user_id", "text", "SPAM", "IRRELEVANTE"])
+f = csv.writer(open("Etc/test.csv", "wb+"))
+f.writerow(["tweet_id", "search_key", "clear_text", "SPAM", "IRRELEVANTE"])
 for x in allTweets:
+    print x
     f.writerow([x["tweet_id"],
                 x["search_key"],
-                x["retweet_count"],
-                x["favorite_count"],
-                x["user_id"],
-                x["text"]])
+                unicode(x["clear_text"])])
