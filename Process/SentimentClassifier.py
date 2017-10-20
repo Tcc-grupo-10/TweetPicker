83a8e0314fdaca689f1af717502ecf4e5bffee5e
ericcampa <ericcampanatti@hotmail.com>
2017-10-20 03:51:08 -0200

    POS Taggin

--- Process/SentimentClassifier.py
+++ Process/SentimentClassifier.py
@@ -1,14 +1,20 @@
-from Databases import Database
-
+#from Databases import Database
+#from Services import WordClassifier
+from nltk.tokenize import TweetTokenizer
+from nltk import pos_tag
 
 def run():
 
-    sentiments = [{"happy": 0.5}, {"sad": 0.01}]
+   # sentiments = [{"happy": 0.5}, {"sad": 0.01}]
 
-    allTweets = Database.getAll(Database.rawTweets)
-
+#    allTweets = Database.getAll(Database.rawTweets)
+    allTweets ={"I am going to the market to buy vegetables and some fruits","sad sad"}
+    tknzr = TweetTokenizer()
     for tweet in allTweets:
         # TODO -> JUST DO IT! MAKE THOSE SENTIMENT COME TRUE!
-        tweet
+        tokens = pos_tag(tknzr.tokenize(tweet))
+        print tokens
 
-    return sentiments
+    #return sentiments
+
+run()
