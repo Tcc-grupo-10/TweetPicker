afa4593b989f8bcf98a044d40b1747db63cc3335
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2017-10-27 01:28:41 -0200

    Python3 fixes

--- Scripts/TableToCSV.py
+++ Scripts/TableToCSV.py
@@ -3,8 +3,8 @@ from Databases import Database
 
 allTweets = Database.getAll(Database.unTweeterizeTable)
 
-f = csv.writer(open("Etc/test.csv", "wb+"))
-f.writerow(["tweet_id", "search_key", "clear_text", "SPAM", "IRRELEVANTE"])
+f = csv.writer(open('Etc/test.csv', 'w', newline=''))
+f.writerow(["tweet_id", "search_key", "clear_text", "SPAM"])
 for x in allTweets:
     print(x)
     f.writerow([x["tweet_id"],
