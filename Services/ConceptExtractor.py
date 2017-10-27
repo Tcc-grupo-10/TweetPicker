4ec61fdad2ceac38b5eac711c7f4d7050497be38
ericcampa <ericcampanatti@hotmail.com>
2017-10-26 22:56:46 -0200

    ConceptExtrator comentado

--- Services/ConceptExtractor.py
+++ Services/ConceptExtractor.py
@@ -1,15 +1,9 @@
-# from Databases import Database
 from nltk.tag.stanford import StanfordPOSTagger
 from nltk.stem import WordNetLemmatizer
-from nltk.parse import stanford
 import os
 from nltk.parse.stanford import StanfordParser
 import re
 from nltk.corpus import wordnet
-from nltk.util import ngrams
-
-
-from nltk.parse.stanford import StanfordDependencyParser
 
 class InstanceTweet:
     def __init__(self, text):
@@ -21,14 +15,12 @@ class InstanceTweet:
         self.tweetTree = []
         self.ngram = []
 
-
     def emojiExtractor(self, text):
         self.emoji = re.findall(":\w+:", text)
         for character in self.emoji:
             if character == " ":
                 return text
         self.text = " ".join(re.sub(r":\w+:", '', text).split())
-
 
 
 def get_wordnet_pos(treebank_tag):
@@ -46,56 +38,67 @@ def get_wordnet_pos(treebank_tag):
 
 def run():
     # Variaveis de ambiente necessarias!
+    #===============================================================================================================
+    #Local do Java JDK (Sim, precisa instalar o JDK)
     java_path = "C:\ProgrA~1\Java\jdk1.8.0_151"
     os.environ['JAVAHOME'] = java_path
-    os.environ['STANFORD_PARSER'] = 'C:\Progra~1\stanford-parser-full-2017-06-09\stanford-parser.jar'
-    os.environ['STANFORD_MODELS'] = 'C:\Progra~1\stanford-parser-full-2017-06-09\stanford-parser-3.8.0-models.jar'
-    #model_path = 'C:\Progra~1\stanford-parser-full-2017-06-09\englishPCFG.ser.gz'
-    #parser = stanford.StanfordParser(model_path=model_path)
+    #Localizar os .jar's do stanfordParser
     parser = StanfordParser('C:\Progra~1\stanford-parser-full-2017-06-09\stanford-parser.jar',
                             'C:\Progra~1\stanford-parser-full-2017-06-09\stanford-parser-3.8.0-models.jar')
-
-    tokenPunctuation = r"""!.,;()+"""
-
-    qallTweets = []
-    tweetList = ["My dog also likes eating sausage."]
-
-    allTweets = [InstanceTweet(tweet) for tweet in tweetList]
-    # TODO -> Resolver problemas em lemmatizar tokens que nao sao apenas letras
+    #Localizar o engish tagger e o .jar do stanfordPOSTagger
     st = StanfordPOSTagger('C:\Progra~1\stanford-postagger\models\english-bidirectional-distsim.tagger',
                            'C:\Progra~1\stanford-postagger\stanford-postagger.jar')
     lemmatizer = WordNetLemmatizer()
-    for indexTweet, tweet in enumerate(allTweets):
-        # EmojiExtractor
+    # ===============================================================================================================
+    #String para adicionar espaços entre os caracteres contidos nela
+    tokenPunctuation = r"""!.,;()+"""
+    #Tweets que serão utilizados
+    tweetList = ["I am going to buy some fruits and some vegetables.","Segundo tweet", "Terceiro Tweet"]
+    #Todas as strings da Lista de strings tweetList serão instanciadas na lista de objetos allTweets
+    allTweets = [InstanceTweet(tweet) for tweet in tweetList]
+    #Para todos as strings do objectList allTweets:
+    for tweet in allTweets:
+        # Extrai emojis e guarda no objeto.emoji
         tweet.emojiExtractor(tweet.text)
-        #Insere espacos entre as pontuacoes para nao interferir no lemmatize
+        #Insere espacos entre os caracteres da variavel tokenPunctuation para nao interferir no lemmatize
         tweet.text = tweet.text.translate(str.maketrans({key: " {0} ".format(key) for key in tokenPunctuation}))
-        # Transforma tweets em tokens
+        # Transforma tweets em tuplas [palavra,Tag referente a palavra] para cada palavra/caractere contida na string. Lista de tags está no documento 'Analise de Sentimento - SenticComputing - ConceptExtractor.docx' da Aline.
         tweet.tweetTokenized = st.tag(tweet.text.split())
-        # faz a lemmatizacao das palavras
+        #Faz a lemmatizacao das palavras para cada tupla na lista de tuplas do tweetTokenized
         for word, tag in tweet.tweetTokenized:
+            #Determina função da palavra na frase de acordo com sua tag de acordo com o metodo get_wordnet_pos()
             wntag = get_wordnet_pos(tag)
+            #Apenas ADV, NOUN, VERB e ADJ devem ser informados como segundo parametro. Caso não seja nenhum desses casos, o segundo parametro deverá ser None.
             if wntag is None:  # not supply tag in case of None
                 tweet.tweetLemmatized.append(lemmatizer.lemmatize(word))
             else:
                 tweet.tweetLemmatized.append(lemmatizer.lemmatize(word, pos=wntag))
-        # Transforma as palavras lemmatizadas em uma unica string
+        # Transforma a lista de palavras lemmatizadas em uma unica string
         tweet.tweetLemmatized = " ".join(tweet.tweetLemmatized)
-        # Seta a arvore do tweet no objeto
-        tweet.tweetTree = list(parser.raw_parse(tweet.tweetLemmatized))
-        # Interface tweetTree para demonstracao
+        # Faz um cast da arvore retornada para o tipo List e a insere no objeto
+        tweet.tweetTree = list(parser.raw_parse(tweet.tweetLemmatized))[0]
+        #Função mágica que consegue pegar nós da arvore referente ao seu label no filter de lambda
+        tweet.tweetTree = [[" ".join(vp.leaves()), "".join(vp.label())] for vp in list(tweet.tweetTree.subtrees(filter=lambda x:  x.label() == 'NP'))]
+
+        #Criação de bigramas para utilizar nos resultados obtidos com o chunk da tweetTree
+        #Cria string com palavras da tweetTree para o bigram
+        """
+        nounphrase = ''
+        for np,token in tweet.tweetTree:
+            #if np not in nounphrase:
+            nounphrase += " " + np
+
+
+        bigrams = ngrams(nounphrase.split(),2)
+
+        for grams in bigrams:
+            print(grams)
+        """
+        # Interface tweetTree para ilustração da arvore
         """for line in tweet.tweetTree:
             for tweet.tweetTree in line:
                 tweet.tweetTree.draw()"""
-
-        #print(tweet.tweetTree)
-        tweet.tweetTree = list(parser.raw_parse(tweet.tweetLemmatized))
-
-        """for line in tweet.tweetTree:
-            for tweet.tweetTree in line:"""
-
-
-
     return allTweets
 
+#Inicializa o método run()
 run()
