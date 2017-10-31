1427e02a6951abaaae29b9c82e0487bcd2980bff
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2017-10-31 16:07:12 -0200

    SenticNet class

--- Classifier/sentimentClassifier.py
+++ Classifier/sentimentClassifier.py
@@ -1,11 +1,20 @@
 from Classifier.conceptExtractor import ConceptExtractor
+from Services.SenticNet import SenticNet
 
 class SentimentClassifier(object):
+
+    def __init__(self):
+        self.senticNet = SenticNet()
 
     def run(self, tweet):
         concept_extractor = ConceptExtractor()
         #tweet = "i just love brown. gots7"
-        tweet.sentiment = concept_extractor.extract_list_of_event_concepts(tweet.preprocessedTweet)
+        concept_extractor.extract_list_of_event_concepts(tweet.preprocessedTweet)
         #concept_extractor.extract_list_of_object_concepts()
+
+        # TODO -> get concepts from concept_extractor
+        concepts = ["go_market", "fresh_fruit", "bad_feeling", "dont_know"]
+
+        tweet.sentiment = self.senticNet.getInfoList(concepts)
 
 # run()
--- Services/SenticNet.py
+++ Services/SenticNet.py
@@ -1,30 +1,35 @@
 from senticnet.senticnet import Senticnet
 
-def getInfo(concept):
-    try:
-        sn = Senticnet()
-        concept_info = sn.concept(concept)
-        polarity_value = sn.polarity_value(concept)
-        polarity_intense = sn.polarity_intense(concept)
-        moodtags = sn.moodtags(concept)
-        semantics = sn.semantics(concept)
-        sentics = sn.sentics(concept)
+class SenticNet(object):
 
-        """print(concept)
-        print("concept_info: {}".format(concept_info))
-        print("polarity_value: {}".format(polarity_value))
-        print("polarity_intense: {}".format(polarity_intense))
-        print("moodtags: {}".format(moodtags))
-        print("semantics: {}".format(semantics))
-        print("sentics: {}".format(sentics))
-        print("\n\n")"""
-        return "{} - {}".format(polarity_value, polarity_intense)
-    except:
-        return "NOT POSSIBLE"
+    def getInfo(self, concept):
+        try:
+            sn = Senticnet()
+            concept_info = sn.concept(concept)
+            polarity_value = sn.polarity_value(concept)
+            polarity_intense = sn.polarity_intense(concept)
+            moodtags = sn.moodtags(concept)
+            semantics = sn.semantics(concept)
+            sentics = sn.sentics(concept)
 
-def getInfoList(concepts):
-    for concept in concepts:
-        info = getInfo(concept)
-        print("{}: {}".format(concept, info))
+            """print(concept)
+            print("concept_info: {}".format(concept_info))
+            print("polarity_value: {}".format(polarity_value))
+            print("polarity_intense: {}".format(polarity_intense))
+            print("moodtags: {}".format(moodtags))
+            print("semantics: {}".format(semantics))
+            print("sentics: {}".format(sentics))
+            print("\n\n")"""
+            return "{} - {}".format(polarity_value, polarity_intense)
+        except:
+            return "NOT POSSIBLE"
 
-getInfoList(["go_market", "fresh_fruit", "bad_feeling", "dont_know"])
+    def getInfoList(self, concepts):
+        infos = []
+        for concept in concepts:
+            info = self.getInfo(concept)
+            infos.append(info)
+            print("{}: {}".format(concept, info))
+        return infos
+
+# SenticNet().getInfoList(["go_market", "fresh_fruit", "bad_feeling", "dont_know"])
