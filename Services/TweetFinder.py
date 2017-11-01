910bb999a703205ed6e800c03f17f684f5253682
Gabriel da Silva Alves <gabrielonze@hotmail.com>
2017-11-01 02:54:08 -0200

    fix

--- Services/TweetFinder.py
+++ Services/TweetFinder.py
@@ -25,7 +25,7 @@ def findTweets(id,bearerToken):
 
 def run():
     tokenUserless = TwitterIntegration.getTokenUserless()
-    workbook = xlrd.open_workbook("C:\\Users\\EricContreraCampanat\\Documents\\TCC\\journal.pone.0182487.s003.xlsx")
+    workbook = xlrd.open_workbook("../Etc/journal.pone.0182487.s003.xlsx")
     sheets = workbook.sheet_names()
     tweetList = []
     for sheet_name in sheets:
@@ -34,52 +34,45 @@ def run():
             row_valaues = sh.row_values(rownum)
             tweetList.append((row_valaues[0], int(row_valaues[1])))
     # Todas as strings da Lista de strings tweetList serão instanciadas na lista de objetos allTweets
-    allTweets = [TweetFinder(tweetId,label) for (tweetId,label) in tweetList]
+    allTweets = [TweetFinder(tweetId, label) for (tweetId, label) in tweetList]
 
-    fi = open("C:\\Users\\EricContreraCampanat\\Documents\\TCC\\test.csv", "w", newline='')
+    fi = open("../Etc/spamList.csv", "w", newline='')
     f = csv.writer(fi)
     f.writerow(["tweet_id", " label", " text"])
     fi.flush()
     tweetApiList = [allTweets[i:i+100] for i in range(0, len(allTweets), 100)]
     for tweets in tweetApiList:
-        #try:
-        tweet = []
-        for x in tweets:
-            tweet.append(x.id)
-            # twitter.com/anyuser/status/
-        tweet = ",".join(tweet)
-        jsonO = findTweets(tweet, tokenUserless)
-        jsonStr = []
-        for jIndex, json in enumerate(jsonO):
-            jsonStr.append(json['id'])
+        try:
+            tweet = []
+            for x in tweets:
+                tweet.append(x.id)
+                # twitter.com/anyuser/status/
+            tweet = ",".join(tweet)
+            jsonO = findTweets(tweet, tokenUserless)
+            jsonStr = []
+            for jIndex, json in enumerate(jsonO):
+                jsonStr.append(json['id'])
 
-            tweet = list(filter(lambda x: x.id == json['id_str'], tweets))
-            if(len(tweet) == 1):
-                tweet[0].text = TweetCleaner.processTweet(json['text'])
-                print(json['id'])
+                tweet = list(filter(lambda x: x.id == json['id_str'], tweets))
+                if(len(tweet) == 1):
+                    tweet[0].text = TweetCleaner.processTweet(json['text'])
+                    print(json['id'])
 
-            """for tweet in tweets:
-                if json['id_str'] == tweet.id:
-                    tweet.text = json['text'].encode('utf-8')
-                    print(json['id'])"""
+                """for tweet in tweets:
+                    if json['id_str'] == tweet.id:
+                        tweet.text = json['text'].encode('utf-8')
+                        print(json['id'])"""
 
 
-            f.writerow([tweet[0].id, " {}".format(tweet[0].label), " {}".format(tweet[0].text)])
-            fi.flush()
-    """except:
+                f.writerow([tweet[0].id, " {}".format(tweet[0].label), " {}".format(tweet[0].text)])
+                fi.flush()
+        except:
             print(time.localtime())
             time.sleep(60 * 15)
-            continue"""
+            continue
 
     fi.close()
     print("======================================================")
-
-
-
-
-
-
-        # twitter.com/anyuser/status/
 
 
 run()
